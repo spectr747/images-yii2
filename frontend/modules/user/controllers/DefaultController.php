1b4595cf3a0d7cc2c89082ebd4b13fb292ac4658
Spectr Spectrum <spectr747@gmail.com>
2019-11-22 23:11:47 +0400

    settings auth

--- frontend/modules/user/controllers/DefaultController.php
+++ frontend/modules/user/controllers/DefaultController.php
@@ -10,7 +10,7 @@ use yii\web\BadRequestHttpException;
 use yii\web\Controller;
 use yii\filters\VerbFilter;
 use yii\filters\AccessControl;
-use common\models\LoginForm;
+use frontend\modules\user\models\LoginForm;
 use frontend\modules\user\models\PasswordResetRequestForm;
 use frontend\modules\user\models\ResetPasswordForm;
 use frontend\modules\user\models\SignupForm;
