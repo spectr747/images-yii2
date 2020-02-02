ad8ac04c15aa1b41a82e2ae19a2d2321fedb4231
vagrant <vagrant@i2>
2020-02-02 19:40:09 +0000

    testing posts and users

---
+++ frontend/tests/fixtures/PostFixture.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace frontend\tests\fixtures;
+
+use yii\test\ActiveFixture;
+
+class PostFixture extends ActiveFixture
+{
+
+    public $modelClass = 'frontend\models\Post';
+
+}
+
--- frontend/tests/unit/UserTest.php
+++ frontend/tests/unit/UserTest.php
@@ -1,6 +1,9 @@
 <?php
 namespace frontend\tests;
 
+use Yii;
+use frontend\tests\fixtures\UserFixture;
+
 class UserTest extends \Codeception\Test\Unit
 {
     /**
@@ -8,17 +11,53 @@ class UserTest extends \Codeception\Test\Unit
      */
     protected $tester;
     
-    protected function _before()
+    public function _fixtures()
     {
+        return ['users' => UserFixture::className()];
     }
+    
+//    public function _before()
+//    {
+//        Yii::$app->setComponents([
+//            'redis' => [
+//                'class' => 'yii\redis\Connection',
+//                'hostname' => 'localhost',
+//                'port' => 6379,
+//                'database' => 1,
+//            ],
+//        ]);
+//    }
 
-    protected function _after()
+    
+    public function testGetNicknameOnNicknameEmpty()
     {
+        $user = $this->tester->grabFixture('users', 'user1');
+        expect($user->getNickname())->equals(1);        
     }
-
-    // tests
-    public function testSomeFeature()
+    
+    public function testGetNicknameOnNicknameNotEmpty()
     {
-
+        $user = $this->tester->grabFixture('users', 'user2');
+        expect($user->getNickname())->equals('catelyn');  
     }
+  
+    public function testGetPostCount()
+    {
+        $user = $this->tester->grabFixture('users', 'user1');
+        expect($user->getPostCount())->equals(3);  
+    }
+//    
+//    public function testFollowUser()
+//    {
+//        $user1 = $this->tester->grabFixture('users', 'user1');
+//        $user3 = $this->tester->grabFixture('users', 'user3');
+//        
+//        $user3->followUser($user1);
+//        
+//        $this->tester->seeRedisKeyContains('user:1:followers', 3);
+//        $this->tester->seeRedisKeyContains('user:3:subscriptions', 1);
+//                
+//        $this->tester->sendCommandToRedis('del', 'user:1:followers');
+//        $this->tester->sendCommandToRedis('del', 'user:3:subscriptions');
+//    }
 }
